863d96a92d3e9333c90e646ef8d436c4aeb0390b
robsonsilv4 <robsonsilv410@gmail.com>
2023-08-02 09:49:48 -0300

    refactor(example): update MyApp test

--- example/test/widget_test.dart
+++ example/test/widget_test.dart
@@ -5,25 +5,19 @@
 // gestures. You can also use WidgetTester to find child widgets in the widget
 // tree, read text, and verify that the values of widget properties are correct.
 
+import 'package:example/location_flow/location_flow.dart';
 import 'package:example/main.dart';
-import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
 
 void main() {
-  testWidgets('Counter increments smoke test', (WidgetTester tester) async {
-    // Build our app and trigger a frame.
-    await tester.pumpWidget(const MyApp());
+  group('MyApp', () {
+    late LocationRepository locationRepository;
 
-    // Verify that our counter starts at 0.
-    expect(find.text('0'), findsOneWidget);
-    expect(find.text('1'), findsNothing);
+    setUp(() => locationRepository = LocationRepository());
 
-    // Tap the '+' icon and trigger a frame.
-    await tester.tap(find.byIcon(Icons.add));
-    await tester.pump();
-
-    // Verify that our counter has incremented.
-    expect(find.text('0'), findsNothing);
-    expect(find.text('1'), findsOneWidget);
+    testWidgets('renders Home', (tester) async {
+      await tester.pumpWidget(MyApp(locationRepository: locationRepository));
+      expect(find.byType(Home), findsOneWidget);
+    });
   });
 }
